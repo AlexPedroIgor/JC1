506e85861c5cd5a9c3932b464bc8a92f8cd14092
Pedro da Luz <contato@pedrodaluz.com.br>
2016-06-27 21:34:09 -0300

    Colisão circular com erros

--- Jogo Montado/fisica.c
+++ Jogo Montado/fisica.c
@@ -14,7 +14,7 @@
 #include "config.h"
 
 // Pre carregamento das funcoes de colisao
-int Colisao_Circular(Objeto* Objeto1, Objeto* Objeto2);
+int Colisao_Circular(Objeto* objeto1, Objeto* objeto2);
 int Colisao_Retangular(Objeto* Objeto1, Objeto* Objeto2);
 int Colisao_Perimetro(Objeto* objeto1, Objeto* objeto2);
 int Colisao_LimiteDeTela(Objeto* objeto);
@@ -25,9 +25,136 @@ int Colisao_Impacto_Jogador(Jogador* jogador, Objeto* objeto);
 int Colisao_Impacto_Inimigo(Inimigo* inimigo, Objeto* objeto);
 
 // Sistema de colisao em circulos (verifica tangentes dos circulos)
-int Colisao_Circular(Objeto* Objeto1, Objeto* Objeto2)
+int Colisao_Circular(Objeto* objeto1, Objeto* objeto2)
 {
+	struct
+	{
+		int raio;
+		struct
+		{
+			int x, y;
+		}centro;
+	}circulo1, circulo2;
 
+	circulo1.centro.x = objeto1->posicao.x + (objeto1->frame.w/2);
+	circulo1.centro.y = objeto1->posicao.y + (objeto1->frame.h/2);
+	circulo1.raio = objeto1->frame.w/2;
+
+	circulo2.centro.x = objeto2->posicao.x + (objeto2->frame.w/2);
+	circulo2.centro.y = objeto2->posicao.y + (objeto2->frame.h/2);
+	circulo2.raio = objeto2->frame.w/2;
+
+	int raios = circulo1.raio + circulo2.raio;
+
+	int distancia, quadrante;
+
+	// Calculo da distancia
+
+	// Eixo X
+	if (circulo1.centro.x == circulo2.centro.x)
+	{
+		distancia = abs(circulo1.centro.y - circulo2.centro.y);
+	}
+	// Eixo Y
+	else if (circulo1.centro.y == circulo2.centro.y)
+		{
+			distancia = abs(circulo1.centro.x - circulo2.centro.x);
+		}
+	// Quadrante 1
+	else if (circulo1.centro.x > circulo2.centro.x
+		&& circulo2.centro.y > circulo2.centro.y)
+	{
+		quadrante = 1;
+		distancia = sqrt( pow(circulo1.centro.x - circulo2.centro.x, 2)
+			+ pow(circulo1.centro.y - circulo2.centro.y, 2) );
+	}
+	// Quadrante 2
+	else if (circulo1.centro.x < circulo2.centro.x
+		&& circulo1.centro.y > circulo2.centro.y)
+	{
+		quadrante = 2;
+		distancia = sqrt( pow(circulo2.centro.x - circulo1.centro.x, 2)
+			+ pow(circulo1.centro.y - circulo2.centro.y, 2) );
+	}
+	// Quatrande 3
+	else if (circulo1.centro.x < circulo2.centro.x
+		&& circulo1.centro.y < circulo2.centro.y)
+	{
+		quadrante = 3;
+		distancia = sqrt( pow(circulo2.centro.x - circulo1.centro.x, 2)
+			+ pow(circulo2.centro.y - circulo1.centro.y, 2) );
+	}
+	// Quadrante 4
+	else if (circulo1.centro.x > circulo2.centro.x
+		&& circulo1.centro.y < circulo2.centro.y)
+	{
+		quadrante = 4;
+		distancia = sqrt( pow(circulo1.centro.x - circulo2.centro.x, 2)
+			+ pow(circulo2.centro.y - circulo1.centro.y, 2) );
+	}
+
+	printf("Posicao 1\n");
+	printf("X: %d\nY: %d\n", circulo1.centro.x, circulo1.centro.y);
+	printf("Posicao 2\n");
+	printf("X: %d\nY: %d\n", circulo2.centro.x, circulo2.centro.y);
+	printf("Distancia: %d\n", distancia);
+	printf("Raios: %d\n", raios);
+
+	// Testes de posicao
+
+	// Cima
+	if (objeto1->movimento.cima
+		&& (quadrante == 3 || quadrante == 4))
+	{
+		if (circulo1.centro.y <= circulo2.centro.y)
+		{
+			if (distancia <= raios)
+				return VERDADEIRO;
+			else
+				return FALSO;
+		}
+	}
+
+	// Baixo
+	else if (objeto1->movimento.baixo
+		&& (quadrante == 1 || quadrante == 2))
+	{
+		if (circulo1.centro.y >= circulo2.centro.y)
+		{
+			if (distancia <= raios)
+				return VERDADEIRO;
+			else
+				return FALSO;
+		}
+	}
+
+	// Esquerda
+	else if (objeto1->movimento.esquerda
+		&& (quadrante == 1 || quadrante == 4))
+	{
+		if (circulo1.centro.x <= circulo2.centro.x)
+		{
+			if (distancia <= raios)
+				return VERDADEIRO;
+			else
+				return FALSO;
+		}
+	}
+
+	// Direita
+	else if (objeto1->movimento.direita
+		&& (quadrante == 2 || quadrante == 3))
+	{
+		if (circulo1.centro.x >= circulo2.centro.x)
+		{
+			if (distancia <= raios)
+				return VERDADEIRO;
+			else
+				return FALSO;
+		}
+	}
+	else
+		return FALSO;
 }
 
 // Sistema de colisao de retangulos (verifica se um retangulo esta dentro do outro)
